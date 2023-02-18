dec97cb20117f3868240af66ee2d26243a274179
Enu Azubike <enuazubike88@gmail.com>
2023-02-18 11:45:06 +0100

    Added Bean Validation to the BeerDto

--- src/main/java/com/azubike/msscbeerservice/web/model/BeerDto.java
+++ src/main/java/com/azubike/msscbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,13 +18,13 @@ import java.util.UUID;
 @Data
 @Builder
 public class BeerDto {
-  private UUID id;
-  private String beerName;
-  private Long version;
-  private Long upc;
-  private BigDecimal price;
+  @Null private UUID id;
+  @NotBlank private String beerName;
+  @Null private Long version;
+  @Positive @NotNull private Long upc;
+  @Positive @NotNull private BigDecimal price;
   private Integer quantityOnHand;
-  private OffsetDateTime createdDate;
-  private OffsetDateTime lastModifiedDate;
-  private BeerStyle beerStyle;
+  @Null private OffsetDateTime createdDate;
+  @Null private OffsetDateTime lastModifiedDate;
+  @NotNull private BeerStyle beerStyle;
 }
